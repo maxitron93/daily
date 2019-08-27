d6bc22ec7630c7470a7c9d5855b158d471c5abb9
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-08-27 15:39:21 +1000

    Finish header section basics

--- src/js/app.js
+++ src/js/app.js
@@ -14,13 +14,17 @@ importImages()
 
 // Add state here
 const state = {
-    headingColor: 'green'
+    items: [
+		{type: 'task', xStart: 0, width: 50, color: 1},
+		{type: 'task', xStart: 50, width: 100, color: 2},
+		{type: 'task', xStart: 150, width: 50, color: 3},
+		{type: 'deadline', xStart: 200, color: 5},
+		{type: 'task', xStart: 200, width: 120, color: 4}
+	]
 }
 
 // Add event listeners here
-elements.mainHeader.addEventListener('click', () => {
-    changeHeadingColor(state)
-})
+
 
 
 

5a70c54658249556ec7048f16da0f34d26600890
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-09-02 16:20:36 +1000

    Add laoder

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import 'normalize.css/normalize.css';
 import '../scss/styles.scss';
-import interact from 'interactjs';
 import { initSortableItems } from './Controller/sortableItemController'
 import { importImages } from './importImages'
 import { elements } from './elements'
@@ -10,11 +9,12 @@ import { initInteractiveDeadlines } from './Controller/interactiveDeadlineContro
 // Import something from /src/js/importImages.js so webpack will put the images into /dist/img when building
 importImages()
 
-// Initialize sortable items
+// Initialize interactive objects
 initSortableItems()
-
-// Initialize interactive tasks
 initInteractiveTasks()
-
-// Initialize interactive deadlines
 initInteractiveDeadlines()
+
+// Initialize app
+const initApp = () => {
+    // Get data
+}

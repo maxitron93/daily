a94a17160a9344f2d72bc1f8dcf2078ce57c39c0
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-09-03 19:07:04 +1000

    Change deadling item styling

--- src/js/dummyData.js
+++ src/js/dummyData.js
@@ -1,6 +1,6 @@
 const dummyData = [
     {type: 'task', id: '1', xStart: 0, width: 0.2, color: 3},
-    {type: 'deadline', id: '10', xStart: 0.18, color: 5},
+    {type: 'deadline', id: '10', xStart: 0.2, color: 2},
 ]
 
 export { dummyData }
